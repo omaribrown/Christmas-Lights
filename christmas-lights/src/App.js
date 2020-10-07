9b2b856fea3164abe4bc75e5323026458f5b6d34
Omari Brown <omaribrown@Omaris-MacBook-Air.local>
2020-10-07 12:21:34 -0400

    initial commits

---
+++ christmas-lights/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Christmas Lights!</h1>
+      <div className='ball ball-one'></div>
+      <div className='ball ball-two'></div>
+      <div className='ball ball-one'></div>
+      <div className='ball ball-two'></div>
+
+    </div>
+  );
+}
+
+export default App;
